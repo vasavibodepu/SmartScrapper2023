a37cd1c740a4d80af9828c5ac55e8f7090b70695
vasavibodepu <vasavijb@gmail.com>
2023-12-17 22:19:58 -0600

    Updated pagination logic to fetch pages dynamically. Merged Hypertension changes from Rupali.

--- src/test/java/com/numpy/scraping/team17/BaseRecipeExtractor.java
+++ src/test/java/com/numpy/scraping/team17/BaseRecipeExtractor.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -13,7 +14,10 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
 import com.numpy.scraping.team17.model.Recipe;
@@ -31,6 +35,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
  */
 public class BaseRecipeExtractor {
 
+	private static final String PAGEINDEX_PARAM_NAME = "?pageindex=";
 	private static final String ALLERGIES_SUFFIX = "_Allergies";
 	private static final int ALLERGIES_COLUMN_INDEX = 1;
 	private static final String TO_ADD = "_To_Add";
@@ -47,25 +52,43 @@ public class BaseRecipeExtractor {
 	private static final String ALLERGIES_WORK_SHEET_NAME = "Allergies";
 	private static final String FILE_PATH_ALLERGIES = SRC_TEST_RESOURCES_TEST_DATA_INPUT + "Allergies.xlsx";
 
-	private static final int MAX_RECEIPES_PER_MORBIDITY = 5;
-	private static final int MAX_PAGES_TO_FETCH = 1;
+	private static final int MAX_RECEIPES_PER_MORBIDITY = 1200;
+	private static final int MAX_PAGES_TO_FETCH = 50;
 	protected WebDriver driver = null;
-	
+
 	public BaseRecipeExtractor() {
 		super();
 	}
 
 	@BeforeClass
-	protected void loadHomePage() {
+	protected void loadHomePage() throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		// Configure Chrome options for headless mode
+		ChromeOptions options = new ChromeOptions();
+		// options.addArguments("--headless");
+		options.addArguments("--headless", "--disable-gpu");
+
+		driver = new ChromeDriver(options);
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+
 		driver.get(HOME_PAGE);
 		driver.manage().window().maximize();
+		// driver.manage().timeouts().implicitlyWait(10); // Adjust the timeout
+		// as needed
+
+		Thread.sleep(5000);
 	}
 
 	@AfterClass
 	protected void closeBrower() {
 		driver.quit();
+	}
+
+	@AfterMethod
+	public void getRunTime(ITestResult tr) {
+
+		long time = tr.getEndMillis() - tr.getStartMillis();
+		System.out.println("Total time taken for test :: " + tr.getName() + " :: in (secs) : " + (((time / 1000))));
 	}
 
 	/**
@@ -77,7 +100,7 @@ public class BaseRecipeExtractor {
 		// arraylist to store all the links
 		ArrayList<String> links = new ArrayList<>();
 
-		fetchAllRecipeUrls(links, driver);
+		fetchAllRecipeUrlsFromPagination(links, driver);
 		System.out.println("Morbidity : " + morbidity + " :: Number of Urls to scrape : " + links.size());
 
 		// load each recipe page from above list and populate the recipe object
@@ -85,18 +108,23 @@ public class BaseRecipeExtractor {
 		List<Recipe> allRecipes = new ArrayList<Recipe>();
 
 		for (String recipePageUrl : links) {
+
 			Recipe recipe = new Recipe();
 			recipe.setTargettedMorbidConditions(morbidity);
 			recipe.setRecipeURL(recipePageUrl);
 
-			// populate all other recipe fields from the details page.
-			driver.get(recipePageUrl);
-
 			// read xpaths and values for name, ingredients etc
 			populateRecipe(recipe);
 
-			allRecipes.add(recipe);
+			// System.out.println("recipe : " + recipe);
+
+			if (recipe.getIngredients() != null) {
+				allRecipes.add(recipe);
+			}
 		}
+
+//		System.out.println("allRecipes : " + allRecipes);
+		System.out.println("Exporting recipes for modbidity : " + morbidity);
 
 		// Export allRecipes that does not have eliminated ingredients
 		// into excel <Morbidity>.xlsx . e.g., Hypothyroidism.xlsx
@@ -108,8 +136,10 @@ public class BaseRecipeExtractor {
 			// ingredients using Java Stream API- if ingredients contains filter
 			// list of values
 			boolean foundEliminatedIngredient = eliminateFIlter.stream().anyMatch(recipe.getIngredients()::contains);
+//			System.out.println("foundEliminatedIngredient :  " + foundEliminatedIngredient);
 			if (!foundEliminatedIngredient) {
-				safeList.add(recipe);
+				safeList.add(recipe); // safeList recipes doesn't have
+										// ingredients from eliminated list
 			}
 		}
 
@@ -211,69 +241,87 @@ public class BaseRecipeExtractor {
 	 */
 	private void populateRecipe(Recipe recipe) {
 
-		// Recipe Id
-		String tempRecipeId = recipe.getRecipeURL().substring(recipe.getRecipeURL().lastIndexOf("-") + 1,
-				(recipe.getRecipeURL().length() - 1));
-		recipe.setRecipeID(tempRecipeId);
-
-		// Recipe Name
-		WebElement recipeName = driver.findElement(By.xpath("//span[@id='ctl00_cntrightpanel_lblRecipeName']"));
-		recipe.setRecipeName(recipeName.getText());
-
-		// Recipe URL - already populated
-
-		// //Recipe Category
-		// WebElement recipeCategory = driver.findElement(By.xpath(""));
-
-		// food category
-		// WebElement foodCategory= driver.findElement(By.xpath(""));
-
-		// ingredients
-		List<WebElement> ingredients = driver.findElements(By.xpath("//*[@itemprop=\"recipeIngredient\"]"));
-		StringBuffer ingredientsBuffer = new StringBuffer(256);
-		for (WebElement ingredient : ingredients) {
-			ingredientsBuffer.append(ingredient.getText());
-			ingredientsBuffer.append("\n");
-		}
-		recipe.setIngredients(ingredientsBuffer.toString());
-
-		// Preparation Time
-		WebElement preparationTime = driver.findElement(By.xpath("//time[@itemprop='prepTime']"));
-		recipe.setPreparationTime(preparationTime.getText());
-
-		// cooking Time
-		WebElement cookingTime = driver.findElement(By.xpath("//time[@itemprop='cookTime']"));
-		recipe.setCookingTime(cookingTime.getText());
-
-		// Preparation Method
-		 WebElement preparationMethod =
-		 driver.findElement(By.xpath("//div[@id='recipe_small_steps']"));
-		 recipe.setPreparationmethod(preparationMethod.getText());
-
-		// Nutrient Values
-		// WebElement nutrientValues = driver.findElement(By.xpath(""));
-		// String nutrition =
-		// driver.findElement(By.xpath("//*[@id=\"rcpnuts\"]/span/span")).getText();
-		// System.out.println(nutrition);
-
-		StringBuffer nutritionValues = new StringBuffer();
-		// TODO: make this table dynamic
 		try {
-			for (int i = 1; i <= 3; i++) {
 
-				nutritionValues.append(
-						driver.findElement(By.xpath("//*[@id=\"rcpnutrients\"]/tbody/tr[" + i + "]")).getText());
-				nutritionValues.append("\n");
+			// populate all other recipe fields from the details page.
+			driver.get(recipe.getRecipeURL());
 
+			// Recipe Id
+			String tempRecipeId = recipe.getRecipeURL().substring(recipe.getRecipeURL().lastIndexOf("-") + 1,
+					(recipe.getRecipeURL().length() - 1));
+			recipe.setRecipeID(tempRecipeId);
+
+			try {
+				// Recipe Name
+				WebElement recipeName = driver.findElement(By.xpath("//span[@id='ctl00_cntrightpanel_lblRecipeName']"));
+				recipe.setRecipeName(recipeName.getText());
+			} catch (NoSuchElementException e) {
 			}
+
+			// Recipe URL - already populated
+
+			// //Recipe Category
+			// WebElement recipeCategory = driver.findElement(By.xpath(""));
+
+			// food category
+			// WebElement foodCategory= driver.findElement(By.xpath(""));
+
+			// ingredients
+			try {
+				List<WebElement> ingredients = driver.findElements(By.xpath("//*[@itemprop=\"recipeIngredient\"]"));
+				StringBuffer ingredientsBuffer = new StringBuffer(256);
+				for (WebElement ingredient : ingredients) {
+					ingredientsBuffer.append(ingredient.getText());
+					ingredientsBuffer.append("\n");
+				}
+				recipe.setIngredients(ingredientsBuffer.toString());
+			} catch (NoSuchElementException e) {
+			}
+
+			// Preparation Time
+			try {
+				WebElement preparationTime = driver.findElement(By.xpath("//time[@itemprop='prepTime']"));
+				recipe.setPreparationTime(preparationTime.getText());
+			} catch (NoSuchElementException e) {
+			}
+			// cooking Time
+			try {
+				WebElement cookingTime = driver.findElement(By.xpath("//time[@itemprop='cookTime']"));
+				recipe.setCookingTime(cookingTime.getText());
+			} catch (NoSuchElementException e) {
+			}
+			// Preparation Method
+			try {
+				WebElement preparationMethod = driver.findElement(By.xpath("//div[@id='recipe_small_steps']"));
+				recipe.setPreparationmethod(preparationMethod.getText());
+			} catch (NoSuchElementException e) {
+			}
+
+			// Nutrient Values
+			StringBuffer nutritionValues = new StringBuffer();
+			// TODO: make this table dynamic
+			try {
+				for (int i = 1; i <= 3; i++) {
+
+					nutritionValues.append(
+							driver.findElement(By.xpath("//*[@id=\"rcpnutrients\"]/tbody/tr[" + i + "]")).getText());
+					nutritionValues.append("\n");
+
+				}
+			} catch (NoSuchElementException e) {
+			}
+
+			recipe.setNutrientValues(nutritionValues.toString());
+
+			// Targeted Morbid Conditions - Already populated
+			// WebElement targettedMorbidConditions = driver
+			// .findElement(By.xpath("//a[@id='ctl00_cntleftpanel_ttlhealthtree_tvTtlHealtht211']"));
+
 		} catch (Exception e) {
+
+			System.err.println(
+					"Error while scraping pages for URL : " + recipe.getRecipeURL() + " :: Error : " + e.getMessage());
 		}
-
-		recipe.setNutrientValues(nutritionValues.toString());
-
-		// Targeted Morbid Conditions - Already populated
-		// WebElement targettedMorbidConditions = driver
-		// .findElement(By.xpath("//a[@id='ctl00_cntleftpanel_ttlhealthtree_tvTtlHealtht211']"));
 
 	}
 
@@ -311,26 +359,36 @@ public class BaseRecipeExtractor {
 	 * @param allLInks
 	 * @param driver
 	 */
-	private void fetchAllRecipeUrls(ArrayList<String> allLInks, WebDriver driver) {
+	private void fetchAllRecipeUrlsFromPagination(ArrayList<String> allLInks, WebDriver driver) {
 		// list of WebElements that store all the links
 		WebElement pagination = driver.findElement(By.xpath("//div[@id='pagination']"));
 		List<WebElement> links = pagination.findElements(By.tagName("a"));
-		List<String> pageLinks = new ArrayList<String>();
 
 		// loop through page links to fill the all links
 		Iterator<WebElement> linksIter = links.iterator();
-		int pageCounter = 0;
-		while ((linksIter.hasNext()) && (pageCounter < MAX_PAGES_TO_FETCH)) {
+		int maxPageSize = 1;
+
+		while ((linksIter.hasNext()) && (maxPageSize < MAX_PAGES_TO_FETCH)) {
+
 			WebElement paginationLink = (WebElement) linksIter.next();
-			String pageToLoad = paginationLink.getAttribute("href");
-			pageLinks.add(pageToLoad);
-
-			pageCounter++;
-
+			try {
+				int tempPageNumber = Integer.valueOf(paginationLink.getText());
+				if (tempPageNumber > maxPageSize) {
+					maxPageSize = tempPageNumber;
+				}
+			} catch (NumberFormatException e) {
+			}
 		}
 
-		for (String pageToLoad : pageLinks) {
-			driver.get(pageToLoad);
+		System.out.println("Total Pages : " + maxPageSize);
+
+		// Generate pagination URLs
+		String basePaginationUrl = driver.getCurrentUrl() + PAGEINDEX_PARAM_NAME;
+		List<String> pageLinks = new ArrayList<String>();
+
+		for (int i = 1; i <= maxPageSize; i++) {
+			// System.out.println("loading page : " + basePaginationUrl + i);
+			driver.get(basePaginationUrl + i);
 			populateRecipeLinks(allLInks);
 		}
 
--- src/test/java/com/numpy/scraping/team17/Hypertension.java
+++ src/test/java/com/numpy/scraping/team17/Hypertension.java
@@ -1,26 +1,11 @@
 package com.numpy.scraping.team17;
-
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+public class Hypertension extends BaseRecipeExtractor{
 
-
-/**
- * 
- * @author vbodepu Scrape Hypothyroidism Recipes from TarlaDalal.com
- *
- */
-public class Hypothyroidism extends BaseRecipeExtractor {
-
-	/**
-	 * Export Recipes list for Hypothyroidism
-	 * 
-	 * @throws InterruptedException
-	 */
+	
 	@Test
 	public void scrapHypothyroidismRecipes() throws InterruptedException {
 
@@ -29,12 +14,12 @@ public class Hypothyroidism extends BaseRecipeExtractor {
 
 		// find and click the link for morbidity (e.g., hypothyroidism) from
 		// diet recipes page
-		WebElement hypothyroidismRecipesLink = driver
-				.findElement(By.xpath("//a[@id='ctl00_cntleftpanel_ttlhealthtree_tvTtlHealtht211']"));
-		hypothyroidismRecipesLink.click();
+		WebElement highbloodpreasureRecipesLink = driver
+				.findElement(By.xpath("//a[@id='ctl00_cntleftpanel_ttlhealthtree_tvTtlHealtht152']"));
+		highbloodpreasureRecipesLink.click();
 
 		// Export all recipes for morbidity
-		exportRecipes("Hypothyroidism", 2, 3);
+		exportRecipes("Hypertension", 4, 5);
 	}
 
 	/**
